19c5192cfac5b8209b4b21bd6566f32abcc4e014
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2020-09-23 00:24:14 +0200

    CORS added

--- api/serializers.py
+++ api/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from .models import *
 
 
-class LessonSerializer(serializers.HyperlinkedModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
 
 
-class FlashcardSerializer(serializers.HyperlinkedModelSerializer):
+class FlashcardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flashcard
         fields = '__all__'
